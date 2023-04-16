9c3ac8c89471adf395c0714859992bdcd709c3cc
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-16 22:38:55 +0530

    init changes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
-export default function Home() {
-  return <main className='text-3xl font-bold underline'>Hello world</main>;
-}
+const Home = () => (
+  <main className="text-3xl font-bold underline">Hello world</main>
+);
+export default Home;
