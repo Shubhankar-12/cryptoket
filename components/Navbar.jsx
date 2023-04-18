c1d82b3356693c980dace7b43e8a03d95e2df451
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-18 12:38:26 +0530

    navbar updated imports

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Link from 'next/link';
+import Image from 'next/image';
+import images from '../assets';
 
-const Navbar = () => (
-  <div>Navbar</div>
-);
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  return (<nav>Navbar</nav>);
+};
 
 export default Navbar;
