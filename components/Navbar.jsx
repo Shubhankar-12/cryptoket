a267fb509214099b8cd4787e4c764f1e1c603818
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-18 12:44:17 +0530

    added classes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,13 @@ import images from '../assets';
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
-  return (<nav>Navbar</nav>);
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b
+   dark:bg-nft-dark bg-white
+   dark:border-nft-dark-black-1 border-nft-gray-1"
+    >Navbar
+    </nav>
+  );
 };
 
 export default Navbar;
