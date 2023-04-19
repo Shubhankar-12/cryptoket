995d5923eeb5207427b55401bf406cb772d325d6
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-19 22:34:21 +0530

    nav responsive

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,10 @@
 
-const Button = ({ btnName, classStyles }) => (
-  <button type="button" className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold ${classStyles}`}>
+const Button = ({ btnName, classStyles, handleClick }) => (
+  <button
+    type="button"
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold ${classStyles}`}
+    onClick={handleClick}
+  >
     {btnName}
   </button>
 );
