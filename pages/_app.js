00b940c9c0b374d682b550be662cb71918ac870b
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-21 12:44:08 +0530

    div to component

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,9 @@ const App = ({ Component, pageProps }) => (
   <ThemeProvider attribute="class">
     <div className="dark:bg-nft-dark bg-white min-h-screen">
       <Navbar />
-      <Component {...pageProps} />
+      <div className="pt-65">
+        <Component {...pageProps} />
+      </div>
       <Footer />
     </div>
     <Script src="https://kit.fontawesome.com/daa14cbd41.js" crossorigin="anonymous" />
