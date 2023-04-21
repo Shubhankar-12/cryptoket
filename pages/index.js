0f977cf59eb23285cb769ead9640002c8b021f43
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-21 12:24:11 +0530

    added banner

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
+import { Banner } from '../components';
+
 const Home = () => (
-  <main className="text-3xl font-bold underline">Hello world</main>
+  <div className="flex justify-center sm:px-4 p-12">
+    <div className="w-full minmd:w-4/5">
+      <Banner />
+    </div>
+  </div>
 );
 export default Home;
