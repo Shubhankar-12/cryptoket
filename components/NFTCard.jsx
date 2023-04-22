ee2978ba44a94b27f7f425a9090d9f77c89c1862
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-22 22:45:24 +0530

    nft passes as query

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -4,7 +4,7 @@ import images from '../assets/index';
 
 const NFTCard = ({ nft }) => (
   <div>
-    <Link href={{ pathname: '/nft-details', query: { nft } }}>
+    <Link href={{ pathname: '/nft-details', query: nft }}>
       <div className="flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327
        dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md"
       >
