c43f8f801dfeea4ccb08cf89ea3a023e52777c71
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-04-28 11:32:10 +0530

    Input Done

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Input = () => (
-  <div>Input</div>
+const Input = ({ inputType, title, placeholder, handleClick }) => (
+  <div className="mt-10 w-full">
+    <p className=" font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
+      {title}
+    </p>
+    <input
+      className="dark:bg-nft-black-1 bg-white border dark:border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none
+     font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-3 py-3"
+      placeholder={placeholder}
+      onChange={handleClick}
+    />
+  </div>
 );
 
 export default Input;
