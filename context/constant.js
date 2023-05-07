b2a937826bf715e2d9a0a54379af41ce007a8fdc
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-05-07 22:38:35 +0530

    abis rem

--- context/constant.js
+++ context/constant.js
@@ -2,5 +2,4 @@ import market from './NFTMarketplace.json';
 // address of contract
 export const marketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
-// abi of the contract
 export const marketAddressABI = market.abi;
