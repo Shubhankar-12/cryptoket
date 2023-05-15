7e4c6a78f8633c3075e32003c4b32cfcfa44d937
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-05-15 12:45:39 +0530

    nft provider wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
 import { ThemeProvider } from 'next-themes';
 import Script from 'next/script';
 
+import { NFTProvider } from '../context/NFTContext';
 import '../styles/globals.css';
 import { Navbar, Footer } from '../components';
 
 const App = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
-        <Component {...pageProps} />
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-    <Script src="https://kit.fontawesome.com/daa14cbd41.js" crossorigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/daa14cbd41.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 export default App;
