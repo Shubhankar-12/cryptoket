9b847328c4c6b7f1f4dede02ac55f0f551cc1b49
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-05-15 12:25:18 +0530

    Nftcontext created and basic import

---
+++ context/NFTContext.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+import { marketAddress, marketAddressABI } from './constant';
+
